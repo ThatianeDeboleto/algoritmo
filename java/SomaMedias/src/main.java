f30be640b659862da54469d54a624a0902d31b0d
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2022-04-06 10:54:47 -0300

    add projetos aula algoritmos

---
+++ java/SomaMedias/src/main.java
@@ -0,0 +1,32 @@
+
+public class main {
+
+	public static void main(String[] args) {
+			String nomeAlg;
+			int resultadoSoma;
+			float resultadoMedia;
+			float resultadoMedia2;
+			
+			nomeAlg = "Soma e Media da Ane";
+			resultadoSoma = 10 + 8 + 9 + 15;
+			resultadoMedia = (10+8+9+15)/4;
+		//OU
+			resultadoSoma = 10 + 8 + 9 + 15;
+			resultadoMedia2 = resultadoSoma/4;
+			
+			System.out.println("Soma e média dos numeros 10, 8, 9, 15");
+			System.out.println("Nome do Algoritmo: ");
+			System.out.println(nomeAlg);
+			System.out.println("Resultado da Soma: ");
+			System.out.println(resultadoSoma);
+			System.out.println("Resultado da média: ");
+			System.out.println(resultadoMedia);
+			System.out.println("Resultado da Média usando a variável resultadoSoma: ");
+			System.out.println(resultadoMedia2);
+		}
+
+
+
+
+	}
+
