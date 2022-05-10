5a53511ae7534514a8c9510e17a51f6017b6eb34
Thatiane Rosa Gomes Deboleto <91338088+ThatianeDeboleto@users.noreply.github.com>
2022-05-10 14:15:39 -0300

    Create main.java

---
+++ java/main.java
@@ -0,0 +1,85 @@
+class Programa
+  
+  public static void main (String entrada[])
+{
+  int idade = 26;
+  char genero = "F";
+  double peso = 47.3;
+  String nome = "Thatiane Deboleto";
+  String curso = "Ciencia da computacao";
+  
+  System.out.println("Eu sou a "+nome + "tenho" + idade + "anos e tenho " + peso + "kg de peso e curso" + curso);
+  
+  System.exit(0);
+}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import javax.swing.*;
+
+public static void main(String entrada[ ])
+
+
+
+int n1, n2, div;
+
+double pot;
+
+String msg="";
+
+
+
+n1 = Integer.parseInt(JoptionPane.showInputDialog("Digite um numero inteiro: "));
+
+n2 = Integer.parseInt(JoptionPane.showInputDialog("Digite um outro numero inteiro: "));
+
+
+
+div = (int)n1 / (int)n2;
+
+pot = Math.pow(n1,n2);
+
+
+
+msg = msg + "quociente da divisao de " + n1 + " por " + n2 + " = " + div + "\n";
+
+msg = msg + "potencia de " + n1 + " por " + n2 + " = " + pot + "\n";
+
+
+
+JoptionPane.showInputDialo(null, msg);
+
+
+
+System.exit(0);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import javax.swing.*;
+
+class Atividade
+   public static void main (String entrada [ ])
+{
+int n1, n2, p=i;
+  char op=();
+  String msg="", msgEntr="Digite 1 para produto\n Digite 2 para produtoria \n";
+  
+  
+  n1 = Integer.parseInt(JoptionPane.showInputDialog("Digite um numero inteiro: "));
+
+n2 = Integer.parseInt(JoptionPane.showInputDialog("Digite um outro numero inteiro: "));
+  op= (JOptionPane.showInputDialog(msgEntr)).charAt(0);
+  
+  switch(op)
+  {
+    if(op >= '1' && op <= '3');
+  }
+       
+  {
+   JOptionPane.showInputDialog(null, msg); 
+  }
+       System.exit(0);
+  
+    
+}
